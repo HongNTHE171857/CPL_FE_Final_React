4d71e1c90e9efd99f53b8b5a0f834a0bae4e54e3
thetoannn <naotehtgnoud@gmail.com>
2024-04-03 14:04:42 +0700

    2

--- final-react-app/src/App.js
+++ final-react-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Banner from './components/Banner';
+import GlobalFeed from './components/GlobalFeed';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Banner/>
+        <GlobalFeed/>
+        <Routes>
+          <Route path='/login' element={<SignIn/>} />
+          <Route path='/register' element={<SignUp/>} />
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
--- final-react-app/src/components/GlobalFeed.jsx
+++ final-react-app/src/components/GlobalFeed.jsx
@@ -1,12 +1,70 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Container, Nav, Row } from "react-bootstrap";
+export const Articles_API = "https://api.realworld.io/api/articles";
 
 const GlobalFeed = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const Articles_API = "https://api.realworld.io/api/articles";
+    fetch(Articles_API)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setArticles(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Container>
       <Row>
         <Col md={9}>
+          {/* <div className="feed-toggle">
+            <Nav className="nav-pills outline-active">
+              <Nav.Item style={{ display: "none" }}>
+                <Nav.Link>Your Feed</Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Nav.Link>Global Feed</Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item className="ng-hide">
+                <Nav.Link className="active">
+                  <i className="ion-pound"></i>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </div> */}
+          <div>
+            <a>
+              {/* <img src="https://api.realworld.io/images/demo-avatar.png"/> */}
+            </a>
+            {articles.map((article, index) => {
+              return (
+                <div>
+                  <p>{articles.author.username}</p>
+                  <p></p>
+                </div>
+              );
+            })}
+          </div>
         </Col>
+        {/* <Col md={3}>
+          <Card className="sidebar">
+            <Card.Body>
+              <Card.Title>Popular Tags</Card.Title>
+              <div className="tag-list">
+                <a>Tag</a>
+              </div>
+              <div className="post-preview">No tags are here... yet.</div>
+            </Card.Body>
+          </Card>
+        </Col> */}
       </Row>
     </Container>
   );
