de33c1e71867b154764f0f8be5670db02bd76daa
thetoannn <naotehtgnoud@gmail.com>
2024-04-12 10:18:49 +0700

    update detail

--- final-react-app/src/components/DetailArticles.jsx
+++ final-react-app/src/components/DetailArticles.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import '../styles/DetailArticles.css';
+import "../styles/DetailArticles.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const DetailArticles = () => {
   const { slug } = useParams();
@@ -10,7 +12,9 @@ const DetailArticles = () => {
   useEffect(() => {
     const fetchArticle = async () => {
       try {
-        const response = await fetch(`https://api.realworld.io/api/articles/${slug}`);
+        const response = await fetch(
+          `https://api.realworld.io/api/articles/${slug}`
+        );
         const data = await response.json();
         setArticle(data.article);
       } catch (error) {
@@ -31,55 +35,61 @@ const DetailArticles = () => {
   };
 
   return (
-    <div className="article-page">
-      <div className="banner">
-        <Container>
-          <h1>{article.title}</h1>
-          <div className="article-meta">
-            <a className="" href={`#@${article.author.username}`}>
-              <Image src={article.author.image} />
-            </a>
-            <div className="info">
-              <a className="author" href={`#@${article.author.username}`}>
-                {article.author.username}
+    <div>
+      <Header/>
+      <div className="article-page">
+        <div className="banner">
+          <Container>
+            <h1>{article.title}</h1>
+            <div className="article-meta">
+              <a className="" href={`#@${article.author.username}`}>
+                <Image src={article.author.image} />
               </a>
-              <span className="date">{formatDate(article.createdAt)}</span>
+              <div className="info">
+                <a className="author" href={`#@${article.author.username}`}>
+                  {article.author.username}
+                </a>
+                <span className="date">{formatDate(article.createdAt)}</span>
+              </div>
+              <span>{article.favoritesCount}</span>
             </div>
-            <span>{article.favoritesCount}</span>
-          </div>
+          </Container>
+        </div>
+        <Container className="page">
+          <Row className="article-content">
+            <Col xs={12}>
+              <div>
+                <p>{article.body}</p>
+              </div>
+              <ul className="tag-list">
+                {article.tagList.map((tag, index) => (
+                  <li key={index} className="tag-default tag-pill tag-outline">
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            </Col>
+          </Row>
+          <hr />
+          <div className="article-actions"></div>
+          <Row>
+            <Col xs={12} md={8} className="offset-md-2">
+              <p>
+                <a className="" href="#login">
+                  Sign in
+                </a>
+                <span>&nbsp;or&nbsp;</span>
+                <a className="" href="#register">
+                  sign up
+                </a>
+                <span>&nbsp;to add comments on this article.</span>
+              </p>
+              <div></div>
+            </Col>
+          </Row>
         </Container>
       </div>
-      <Container className="page">
-        <Row className="article-content">
-          <Col xs={12}>
-            <div>
-              <p>{article.body}</p>
-            </div>
-            <ul className="tag-list">
-              {article.tagList.map((tag, index) => (
-                <li key={index} className="tag-default tag-pill tag-outline">{tag}</li>
-              ))}
-            </ul>
-          </Col>
-        </Row>
-        <hr />
-        <div className="article-actions"></div>
-        <Row>
-          <Col xs={12} md={8} className="offset-md-2">
-            <p>
-              <a className="" href="#login">
-                Sign in
-              </a>
-              <span>&nbsp;or&nbsp;</span>
-              <a className="" href="#register">
-                sign up
-              </a>
-              <span>&nbsp;to add comments on this article.</span>
-            </p>
-            <div></div>
-          </Col>
-        </Row>
-      </Container>
+      <Footer/>
     </div>
   );
 };
