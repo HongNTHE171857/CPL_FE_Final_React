a88135050551e050f258acdaae792761660616b1
thetoannn <naotehtgnoud@gmail.com>
2024-04-15 15:23:51 +0700

    last

--- final-react-app/src/components/Footer.jsx
+++ final-react-app/src/components/Footer.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
 
 function Footer({ appName }) {
   return (
-    <div className="container">
-      <Link to="/" className="logo-font">
-        conduit
-      </Link>
-      <span className="attribution">
-        An interactive learning project from{" "}
-        <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed
-        under MIT.
-      </span>
+    <div className="footer">
+      <div className="container">
+        <Link to="/" className="logo-font">
+          conduit
+        </Link>
+        <span className="attribution">
+          An interactive learning project from{" "}
+          <a href="https://thinkster.io"  style={{color: "rgb(187, 187, 187)"}}>Thinkster</a>. Code &amp; design
+          licensed under MIT.
+        </span>
+      </div>
     </div>
   );
 }
